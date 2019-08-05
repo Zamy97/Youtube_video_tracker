a3eaec88f2254421c6317580c53aa31c603fc6a3
Aktar Zaman <Auz.zamy@gmail.com>
2019-08-05 16:21:32 -0400

    first commit

---
+++ src/video_tracker/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class VideoTrackerConfig(AppConfig):
+    name = 'video_tracker'
---
+++ src/youtube_video_tracker/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.template.loader import get_complete
+
+def home_page(request):
+    context = { "title": "Welcome to Youtube Video Tracker"}
+    return render(request, "home_view.html", context)
